368321383abeeb44bf204163e78bc0a9a787dbe4
Naveen Shekhar <Naveen Shekhar@DESKTOP-C90043O>
2021-07-19 00:31:00 +0530

    Controller and Model Created [Naveen Shekhar]

---
+++ SpringRest/src/main/java/com/springrest/spring/model/Course.java
@@ -0,0 +1,42 @@
+package com.springrest.spring.model;
+
+public class Course {
+
+	private long id;
+	private String title;
+	private String discription;
+
+	// Constructor
+	public Course(long id, String title, String discription) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.discription = discription;
+	}
+
+	// Getter Setter
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDiscription() {
+		return discription;
+	}
+
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+
+}
