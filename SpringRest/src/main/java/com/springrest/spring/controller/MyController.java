17e2e1038cb98158139cee1e81fb79a9831a0b99
Naveen Shekhar <Naveen Shekhar@DESKTOP-C90043O>
2021-07-19 14:08:01 +0530

    Update [Naveen Shekhar]

--- SpringRest/src/main/java/com/springrest/spring/controller/MyController.java
+++ SpringRest/src/main/java/com/springrest/spring/controller/MyController.java
@@ -2,6 +2,7 @@ package com.springrest.spring.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import com.springrest.spring.service.ICourseService;
 @RestController
 public class MyController {
 
+	@Autowired
 	private ICourseService courseService;
 
 	@GetMapping("/home")
